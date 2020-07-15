dc1dd12b0b2a1a2d68091371ef4abc8acc9ef86a
BenBagby <benbagby5@gmail.com>
2020-07-15 17:05:14 -0600

    C# fundamentals-module 1

---
+++ pluralsight/Csharp-Development-Fundamentals/1-Beginner/1-C# Fundamentals(Scott Allen)/gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        // This is a method, named Main
+        // Inside of the parenthesis of the method we have parameters
+        // every parameter has a type and a name of the parameter
+        // the name of this parameter is args
+        // the type of this parameter is string array, the square brakets dictate array
+        {
+            // C# is zero-based indexing
+            // string concatination
+            if(args.Length > 0)
+            {
+                Console.WriteLine("Hello " + args[0] + "!");
+                // string interpolation
+                // inside of curly braces is an expression
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
