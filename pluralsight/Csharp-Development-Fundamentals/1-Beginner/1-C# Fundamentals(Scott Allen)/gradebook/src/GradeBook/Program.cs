596f2cd6812a15c9fd54c168a6565ebb2628efd9
BenBagby <benbagby5@gmail.com>
2020-07-24 12:58:10 -0600

    module 7 complete

--- pluralsight/Csharp-Development-Fundamentals/1-Beginner/1-C# Fundamentals(Scott Allen)/gradebook/src/GradeBook/Program.cs
+++ pluralsight/Csharp-Development-Fundamentals/1-Beginner/1-C# Fundamentals(Scott Allen)/gradebook/src/GradeBook/Program.cs
@@ -9,15 +9,50 @@ namespace GradeBook
         {
             
             var book = new Book("Ben's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+
+            var done = false;
+
+            while(!done)
+            {
+                Console.WriteLine("Enter a grade or 'qq to quit");
+                var input = Console.ReadLine();
+                
+                if(input == "q")
+                {
+                    done = true;
+                    continue;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+
+            }
+            
+            
+
+
             book.GetStatistics();
 
             var stats = book.GetStatistics();
             Console.WriteLine($"The lowest grade is {stats.Low:N1}");
             Console.WriteLine($"The highest grade is {stats.High:N1}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The Letter grade is {stats.Letter}");
 
         }
     }
