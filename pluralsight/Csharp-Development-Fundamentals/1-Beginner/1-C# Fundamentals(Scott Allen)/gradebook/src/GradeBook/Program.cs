6c4a816e76d2c0ce806068fef08a3b2a54394d33
BenBagby <benbagby5@gmail.com>
2020-07-31 16:20:12 -0600

    course complete

--- pluralsight/Csharp-Development-Fundamentals/1-Beginner/1-C# Fundamentals(Scott Allen)/gradebook/src/GradeBook/Program.cs
+++ pluralsight/Csharp-Development-Fundamentals/1-Beginner/1-C# Fundamentals(Scott Allen)/gradebook/src/GradeBook/Program.cs
@@ -7,22 +7,32 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            
-            var book = new Book("Ben's Grade Book");
-            book.GradeAdded += OnGradeAdded;
-            book.GradeAdded += OnGradeAdded;
-            book.GradeAdded -= OnGradeAdded;
+
+            IBook book = new DiskBook("Ben's Grade Book");
             book.GradeAdded += OnGradeAdded;
 
+            EnterGrades(book);
 
+            book.GetStatistics();
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"For the book named {book.Name}");
+            Console.WriteLine($"The lowest grade is {stats.Low:N1}");
+            Console.WriteLine($"The highest grade is {stats.High:N1}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The Letter grade is {stats.Letter}");
+        }
+
+        private static void EnterGrades(IBook book)
+        {
             var done = false;
-
-            while(!done)
+            while (!done)
             {
                 Console.WriteLine("Enter a grade or 'qq to quit");
                 var input = Console.ReadLine();
-                
-                if(input == "q")
+
+                if (input == "q")
                 {
                     done = true;
                     continue;
@@ -33,11 +43,11 @@ namespace GradeBook
                     var grade = double.Parse(input);
                     book.AddGrade(grade);
                 }
-                catch(ArgumentException ex)
+                catch (ArgumentException ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
-                catch(FormatException ex)
+                catch (FormatException ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
@@ -47,20 +57,6 @@ namespace GradeBook
                 }
 
             }
-            
-            
-
-
-            book.GetStatistics();
-
-            var stats = book.GetStatistics();
-
-            Console.WriteLine(Book.CATEGORY);
-            Console.WriteLine($"For the book named {book.Name}");
-            Console.WriteLine($"The lowest grade is {stats.Low:N1}");
-            Console.WriteLine($"The highest grade is {stats.High:N1}");
-            Console.WriteLine($"The average grade is {stats.Average:N1}");
-            Console.WriteLine($"The Letter grade is {stats.Letter}");
         }
 
         static void OnGradeAdded(object sender, EventArgs e)
--- pluralsight/Csharp-Development-Fundamentals/1-Beginner/1-C# Fundamentals(Scott Allen)/gradebook/test/GradeBook.Tests/TypeTests.cs
+++ pluralsight/Csharp-Development-Fundamentals/1-Beginner/1-C# Fundamentals(Scott Allen)/gradebook/test/GradeBook.Tests/TypeTests.cs
@@ -18,7 +18,7 @@ namespace GradeBook.Tests
             log += IncrementCount;
 
             var result = log("Hello");
-            Assert.Equal(3, count);
+            Assert.Equal(36, count);
 
         }
 
@@ -62,9 +62,9 @@ namespace GradeBook.Tests
             Assert.Equal("New Name", book1.Name);
         }
 
-        private void GetBookSetName(ref Book book, string name)
+        private void GetBookSetName(ref InMemoryBook book, string name)
         {
-            book = new Book(name);
+            book = new InMemoryBook(name);
         }
 
        [Fact]
@@ -76,9 +76,9 @@ namespace GradeBook.Tests
             Assert.Equal("Book 1", book1.Name);
         }
 
-        private void GetBookSetName(Book book, string name)
+        private void GetBookSetName(InMemoryBook book, string name)
         {
-            book = new Book(name);
+            book = new InMemoryBook(name);
             book.Name = name;
         }
 
@@ -91,7 +91,7 @@ namespace GradeBook.Tests
             Assert.Equal("New Name", book1.Name);
         }
 
-        private void SetName(Book book, string name)
+        private void SetName(InMemoryBook book, string name)
         {
             book.Name = name;
         }
@@ -131,9 +131,9 @@ namespace GradeBook.Tests
 
             Assert.Same(book1, book2);
         }
-        Book GetBook(string name)
+        InMemoryBook GetBook(string name)
         {
-            return new Book(name);
+            return new InMemoryBook(name);
         }
     }
 }
